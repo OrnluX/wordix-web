354c0c1b54db98328eee397849a92d651fd9f45b
OrnluX <ivantarquini91@gmail.com>
2025-06-12 21:39:15 -0300

    basename

--- src/main.jsx
+++ src/main.jsx
@@ -11,7 +11,7 @@ import './assets/css/index.css'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <BrowserRouter basename="/wordix-web">
         <App />
       </BrowserRouter>
     </Provider>
