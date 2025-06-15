b67e82867eacc9a033c3d1d3d0dc5b1582c8cba7
OrnluX <ivantarquini91@gmail.com>
2025-06-15 00:46:38 -0300

    Cambios layout scroll to top para compatibilidad con hashroutes

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,18 @@
 import { Routes, Route } from 'react-router-dom'
 import Layout from './components/layout/Layout'
 
-import Game from './pages/Game'
 import Welcome from './pages/Welcome'
+import Game from './pages/Game'
+// import Estadisticas from './pages/Estadisticas'
 
 export default function App() {
   return (
     <Routes>
-      <Route
-        path="/"
-        element={
-          <Layout>
-            <Welcome />
-          </Layout>
-        }
-      />
-      <Route
-        path="/jugar"
-        element={
-          <Layout>
-            <Game />
-          </Layout>
-        }
-      />
+      <Route element={<Layout />}>
+        <Route path="/" element={<Welcome />} />
+        <Route path="/jugar" element={<Game />} />
+        {/* <Route path="/estadisticas" element={<Estadisticas />} /> */}
+      </Route>
     </Routes>
   )
 }
--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,21 +1,20 @@
 import Header from './Header'
 import Footer from './Footer'
-
+import { Outlet, useLocation } from 'react-router-dom'
 import { useEffect } from 'react'
-import { useLocation } from 'react-router-dom'
 
-export default function Layout({ children }) {
-  const { pathname } = useLocation()
+export default function Layout() {
+  const location = useLocation()
 
   useEffect(() => {
     window.scrollTo({ top: 0, left: 0, behavior: 'smooth' })
-  }, [pathname])
+  }, [location.hash])
 
   return (
     <div className="py-6 min-h-screen bg-gray-900 flex flex-col">
       <Header />
       <main className="flex-grow px-4 py-6 sm:py-10 flex flex-col items-center text-center space-y-6 text-purple-200">
-        {children}
+        <Outlet />
       </main>
       <Footer />
     </div>
